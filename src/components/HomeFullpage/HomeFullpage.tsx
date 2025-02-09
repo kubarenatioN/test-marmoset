cd2f9becd5478f8ecd8d742d57caf53e8dd66ed4
Nick <kbrn.work@gmail.com>
2025-02-10 02:57:09 +0300

    apply working logic + add footer transition

--- src/components/HomeFullpage/HomeFullpage.tsx
+++ src/components/HomeFullpage/HomeFullpage.tsx
@@ -7,12 +7,15 @@ import ReactFullpage, {
   Item,
 } from '@fullpage/react-fullpage';
 import { clsx } from 'clsx';
-import { FC, useRef, useState } from 'react';
+import { FC, useRef } from 'react';
 import styles from './style.module.scss';
 
-const scaleDownFrom = 'scale-down-from';
-const scaleDownTo = 'scale-down-to';
-// const scaleDown = styles['scale-down'];
+const HIDDEN_SLIDE = 'pr-fp-hidden';
+const ACTIVE_SLIDE = 'pr-fp-active';
+const ANIM_IN_CLASS = 'pr-animating-in';
+const ANIM_OUT_CLASS = 'pr-animating-out';
+const Z_INDEX_ABOVE = 2;
+const Z_INDEX_BELOW = 1;
 
 interface HomeFullpageProps {}
 
@@ -20,66 +23,147 @@ const _anchors = ['one', 'two', 'three', 'footer'];
 
 const HomeFullpage: FC<HomeFullpageProps> = ({}) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const canMove = useRef<boolean>(false);
-
-  const [ready, setReady] = useState(false);
+  const isScrolling = useRef<boolean>(false);
 
   let _api: FullpageApi;
 
-  // const _moveTo = (target: string) => {
-  //   _api.moveTo(target);
-  // };
-
-  let updateAfterLoad: () => void = () => {};
-
-  const beforeLeave = (origin: Item, destination: Item, direction: string) => {
-    if (!ready) {
-      return true;
-    }
-
-    if (canMove.current === false) {
-      canMove.current = true;
-
-      containerRef.current?.classList.add('move', `move-${direction}`);
-      origin.item.classList.add(scaleDownFrom);
-      // destination.item.classList.add(scaleDownTo);
-
+  const afterRender = () => {
+    window.addEventListener('hashchange', () => {
       requestTimeout(() => {
-        const id = String(destination.anchor);
-        console.log('move', id);
-        _api.moveTo(id);
-      }, 600);
+        checkHashNavigation();
+      }, 200);
+    });
 
-      updateAfterLoad = () => {
-        origin.item.classList.remove(scaleDownFrom);
-        containerRef.current?.classList.remove('move', `move-${direction}`);
+    requestAnimationFrame(() => {
+      containerRef.current?.classList.add(styles.inited);
+      const current = _api.getActiveSection().item;
+      // console.log(_api);
 
-        // origin.item.classList.remove(scaleDownFrom);
-        // origin.item.classList.remove('scale-down-from-fixed');
+      // show by default first slide
+      current.classList.add(ACTIVE_SLIDE);
+      if (location.hash === '') {
+        current.classList.add(ANIM_IN_CLASS);
+        current.style.animationName = 'moveFromTop';
+        current.style.zIndex = String(Z_INDEX_ABOVE);
+      }
+    });
+  };
 
-        // destination.item.classList.remove(scaleDownTo);
-      };
-
+  const onLeave = (origin: Item, dest: Item, dir: 'up' | 'down') => {
+    if (isScrolling.current === true) {
       return false;
     }
 
-    console.log('before');
+    isScrolling.current = true;
 
-    return true;
+    slideLeave(origin, dest, dir);
   };
 
-  const onLeave = (origin: Item, destination: Item) => {
-    // origin.item.classList.add('scale-down-from-fixed');
+  const onAnimationPhaseEnd = () => {
+    requestTimeout(() => {
+      isScrolling.current = false;
+    }, 100);
   };
 
-  const afterLoad = () => {
-    if (!ready) {
-      return;
+  const checkHashNavigation = () => {
+    const hash = window.location.hash.replace('#', '');
+
+    if (hash !== '' && hash !== 'footer') {
+      _api.moveTo(hash);
+    } else if (hash === '') {
+      _api.moveTo(1);
     }
 
-    canMove.current = false;
+    console.log(_api.getActiveSection().anchor);
+  };
 
-    updateAfterLoad();
+  const slideLeave = (origin: Item, dest: Item, dir: string) => {
+    const currentSlide = origin.item;
+    const nextSlide = dest.item;
+    const footerQuery = '.' + styles.SectionFooter;
+    // const isNextFooter = false;
+    // const isFooter = false;
+    const isNextFooter = Boolean(dest.item.querySelector(footerQuery));
+    const isFooter = Boolean(origin.item.querySelector(footerQuery));
+
+    // console.log(currentSlide, nextSlide);
+
+    let animIn = '';
+    let animOut = '';
+
+    if (dir === 'up') {
+      animIn = 'moveFromTop';
+    } else {
+      animIn = 'moveFromBottom';
+    }
+    animOut = animIn + 'Out';
+
+    if (isNextFooter) {
+      _api.setLockAnchors(true);
+      nextSlide.classList.add(ACTIVE_SLIDE);
+      // const container = nextSlide.parentElement;
+      const fH = nextSlide.clientHeight;
+
+      currentSlide.style.transition = `transform 400ms`;
+      currentSlide.style.transform = `translate3d(0, -${fH}px, 0)`;
+
+      nextSlide.style.bottom = `-${fH}px`;
+      nextSlide.style.transform = `translate3d(0, -100%, 0)`;
+      nextSlide.style.transition = `transform 400ms`;
+
+      const trEnd = () => {
+        onAnimationPhaseEnd();
+        currentSlide.removeEventListener('transitionend', trEnd);
+        _api.setLockAnchors(false);
+      };
+      currentSlide.addEventListener('transitionend', trEnd);
+    } else if (isFooter) {
+      _api.setLockAnchors(true);
+      // const fH = currentSlide.clientHeight;
+
+      nextSlide.style.transition = `transform 400ms`;
+      nextSlide.style.transform = `translate3d(0, 0, 0)`;
+
+      currentSlide.style.transform = `translate3d(0, 0, 0)`;
+      currentSlide.style.transition = `transform 400ms`;
+
+      const trEnd = () => {
+        nextSlide.style.transition = ``;
+        nextSlide.style.transform = ``;
+        currentSlide.style.bottom = ``;
+        currentSlide.classList.remove(ACTIVE_SLIDE);
+
+        onAnimationPhaseEnd();
+        nextSlide.removeEventListener('transitionend', trEnd);
+        _api.setLockAnchors(false);
+      };
+
+      nextSlide.addEventListener('transitionend', trEnd);
+    } else {
+      nextSlide.classList.add(ANIM_IN_CLASS, ACTIVE_SLIDE);
+      nextSlide.style.animationName = animIn;
+      nextSlide.style.zIndex = String(Z_INDEX_ABOVE);
+
+      nextSlide.onanimationend = (event) => {
+        const { animationName } = event;
+
+        if (animationName === animIn) {
+          currentSlide.classList.remove(ACTIVE_SLIDE);
+
+          // reset animations for both items after next slide animation complete
+          currentSlide.style.animationName = '';
+          nextSlide.style.animationName = '';
+
+          onAnimationPhaseEnd();
+        }
+      };
+
+      currentSlide.classList.add(ANIM_OUT_CLASS);
+      currentSlide.style.animationName = animOut;
+      currentSlide.style.zIndex = String(Z_INDEX_BELOW);
+
+      currentSlide.onanimationend = () => {};
+    }
   };
 
   return (
@@ -88,28 +172,19 @@ const HomeFullpage: FC<HomeFullpageProps> = ({}) => {
         <ReactFullpage
           licenseKey={'asd'}
           anchors={_anchors}
-          navigation={ready}
+          navigation={false}
           navigationTooltips={_anchors}
           credits={{
             enabled: false,
             label: '',
           }}
-          sectionsColor={['pink', 'orange', 'salmon']}
-          scrollingSpeed={600}
-          afterRender={() => {
-            requestAnimationFrame(() => {
-              requestTimeout(() => {
-                containerRef.current?.classList.add(styles.inited);
-                setReady(true);
-              }, 1000);
-            });
-          }}
-          beforeLeave={beforeLeave}
-          afterLoad={afterLoad}
+          // scrollBar={false}
+          sectionsColor={['pink', 'orange', 'salmon', 'bisque']}
+          scrollingSpeed={400}
+          afterRender={afterRender}
           onLeave={onLeave}
           render={({ state, fullpageApi }) => {
             _api = fullpageApi;
-            console.log('render');
 
             return (
               <ReactFullpage.Wrapper>
@@ -131,9 +206,11 @@ const HomeFullpage: FC<HomeFullpageProps> = ({}) => {
                   <p>Section 3</p>
                 </FpSection>
 
-                <div className={clsx('section', 'fp-auto-height')}>
-                  <h1>Hello I'm Autoheight</h1>
-                </div>
+                <FpSection options={{ api: fullpageApi }} footer>
+                  <footer className={clsx(styles.Footer)}>
+                    <h1>Hello I'm Footer</h1>
+                  </footer>
+                </FpSection>
               </ReactFullpage.Wrapper>
             );
           }}
@@ -148,14 +225,27 @@ interface FullpageSectionProps {
   options: {
     api: FullpageApi;
   };
+  footer?: boolean;
 }
 
-const FpSection: FC<FullpageSectionProps> = ({ options, children }) => {
+const FpSection: FC<FullpageSectionProps> = ({ options, children, footer }) => {
   const { api } = options;
 
   return (
-    <div className={`${styles.Section} section`}>
-      <div className={styles.SectionInner}>
+    <div
+      className={clsx(
+        styles.Section,
+        'section',
+        'fp-auto-height',
+        HIDDEN_SLIDE
+      )}
+    >
+      <div
+        className={clsx(
+          styles.SectionInner,
+          footer ? styles.SectionFooter : ''
+        )}
+      >
         {/*  */}
         {children}
       </div>

81325cf369c0d3d7e693acb05e4ef5354653475a
Nick <kbrn.work@gmail.com>
2025-05-14 17:13:47 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
         source: '/:path*', // applies to all routes
         headers: [
           {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
             key: 'Netlify-CDN-Cache-Control',
             value: 'public, max-age=60',
           },

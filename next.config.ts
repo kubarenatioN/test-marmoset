cef1d7beb8f5808ccb38be7fcd1a479c42f81221
Nick <kbrn.work@gmail.com>
2025-05-15 22:49:05 +0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=60, must-revalidate',
+            value: 'public, max-age=0, must-revalidate',
           },
           {
             key: 'Netlify-CDN-Cache-Control',

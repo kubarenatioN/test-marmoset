64fe9dd2b34aac529219f7ef99ff7146df24af3a
Nick <kbrn.work@gmail.com>
2025-05-22 09:52:48 +0300

    test granular headers setup

--- src/components/ProjectBanner/ProjectBanner.tsx
+++ src/components/ProjectBanner/ProjectBanner.tsx
@@ -3,7 +3,7 @@
 import { useDeviceType } from '@/helpers/useDeviceType';
 import { IProjectBanner } from '@/models';
 import Image from 'next/image';
-import { FC, useRef } from 'react';
+import { FC } from 'react';
 import styles from './style.module.scss';
 
 const { BannerImage, BannerVideo, BannerVideoWrapper } = styles;
@@ -11,13 +11,9 @@ const { BannerImage, BannerVideo, BannerVideoWrapper } = styles;
 interface ProjectBannerProps {
   banner: IProjectBanner;
   mobile?: boolean;
-  // mobile: Promise<boolean>;
 }
 
-const ProjectBanner: FC<ProjectBannerProps> = ({ banner, mobile = false }) => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  // const isMobile = mobile;
+const ProjectBanner: FC<ProjectBannerProps> = ({ banner }) => {
   const deviceType = useDeviceType();
 
   const { imgUrl, videoUrl, imgMobileUrl, videoMobileUrl, title } = banner;
@@ -36,12 +32,11 @@ const ProjectBanner: FC<ProjectBannerProps> = ({ banner, mobile = false }) => {
             src={imgUrl}
             alt={title ?? ''}
             sizes='100vw'
-            priority
+            loading='eager'
           />
         ) : videoUrl ? (
           <div className={BannerVideoWrapper}>
             <video
-              ref={videoRef}
               src={videoUrl}
               controls={false}
               autoPlay={true}
@@ -59,12 +54,11 @@ const ProjectBanner: FC<ProjectBannerProps> = ({ banner, mobile = false }) => {
           src={imgMobileUrl}
           alt={title ?? ''}
           sizes='100vw'
-          priority
+          loading='eager'
         />
       ) : videoMobileUrl ? (
         <div className={BannerVideoWrapper}>
           <video
-            ref={videoRef}
             src={videoMobileUrl}
             controls={false}
             autoPlay={true}
